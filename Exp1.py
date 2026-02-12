c8746cb9f2ad748ff5506a050ca8df0367c37e47
trishala1318 <trishalakamble9309@gmail.com>
2026-02-12 11:16:17 +0530

    Add files via upload

---
+++ Exp1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 10:49:44 2026
+
+@author: User
+"""
+name = input("Enter your name :")
+age = input(" Enter your age :")
+student_class = input(" Enter your class:")
+division = input("Enter your division :")
+roll_no = input ("Enter your roll number :")
+batch = input("Enter your batch :")
+phone_no = ("Enter your phone number :")
+blood_group = ("Enter your blood group : ")
+address = ("Enter your address :")
+college = input("Enter your college name :")
+city = ("Enter your city name : ")
+print("\n-----Student Information-----")
+
+
+print("Name:",name)
+print("Age:",age)
+print("Class:",student_class)
+print("Division:",division)
+print("roll no : ",roll_no)
+print("Batch:",batch)
+print("phone no. : ",phone_no)
+print("blood group : ",blood_group)
+print("address : ",address)
+print("College:",college)
+print("city : ",city)
