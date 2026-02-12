8987da9b70d9c6fa6f37af7246e699cc7ae2baa1
trishala1318 <trishalakamble9309@gmail.com>
2026-02-12 11:18:54 +0530

    Add files via upload

---
+++ exp3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 10:29:48 2026
+
+@author: User
+"""
+
+n = input(input("Enter number of rows:"))
+
+for i in range(1,n+1):
+  for j in range(1,i+1):
+       print(j,end="")
+       print()
